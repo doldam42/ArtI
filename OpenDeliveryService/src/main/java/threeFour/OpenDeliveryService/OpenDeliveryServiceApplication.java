9c07a4c07131fc371633f6f2826f18d663c5cbdf
doldam42 <doldam42@github.com>
2023-03-03 13:07:18 +0900

    init

---
+++ OpenDeliveryService/src/main/java/threeFour/OpenDeliveryService/OpenDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package threeFour.OpenDeliveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ OpenDeliveryService/src/test/java/threeFour/OpenDeliveryService/OpenDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package threeFour.OpenDeliveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
