620f6ea6c763e5163663c7553bef39a05a3b3ebe
Sujin Gwak <kasiuky@nate.com>
2023-03-26 21:01:20 +0900

    Book / Styler controller 작성

--- ArtI/src/main/java/threeFour/ArtI/book/BookController.java
+++ ArtI/src/main/java/threeFour/ArtI/book/BookController.java
@@ -1,4 +1,31 @@
 package threeFour.ArtI.book;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class BookController {
+
+    private Map<String, Book> bookMap;
+
+    /* 생성한 동화 리스트 */
+    @GetMapping("/book")
+    public List<Book> getBookList(){
+        return new ArrayList<Book>(bookMap.values());
+    }
+
+    /* 특정 동화 불러오기 */
+    @GetMapping("/book/{id}")
+    public Book getBook(@PathVariable("id") Long id){
+        return bookMap.get(id);
+    }
+
+    /* 특정 동화 삭제하기 */
+    @DeleteMapping("/book/{id}")
+    public void deleteBook(@PathVariable("id") Long id){
+        bookMap.remove(id);
+    }
 }
--- ArtI/src/main/java/threeFour/ArtI/styler/StylerController.java
+++ ArtI/src/main/java/threeFour/ArtI/styler/StylerController.java
@@ -1,4 +1,35 @@
 package threeFour.ArtI.styler;
 
+import lombok.Getter;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class StylerController {
+
+    private Map<String, Styler> stylerMap;
+
+    /* 생성한 이미지 리스트 */
+    @GetMapping("/styler")
+    public List<Styler> getStylerList(){
+        return new ArrayList<Styler>(stylerMap.values());
+    }
+
+    /* 특정 이미지 불러오기 */
+    @GetMapping("/styler/{id}")
+    public Styler getStyler(@PathVariable("id") Long id){
+        return stylerMap.get(id);
+    }
+
+    /* 특정 이미지 삭제하기*/
+    @DeleteMapping("/styler/{id}")
+    public void deleteStyler(@PathVariable("id") Long id){
+        stylerMap.remove(id);
+    }
 }
