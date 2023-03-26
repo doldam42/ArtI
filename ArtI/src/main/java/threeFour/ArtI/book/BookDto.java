7d2d4ba06d091dc5060999f491cc052209c48b80
Sujin Gwak <kasiuky@nate.com>
2023-03-26 21:46:22 +0900

    Book / Styler Dto 작성

--- ArtI/src/main/java/threeFour/ArtI/book/BookDto.java
+++ ArtI/src/main/java/threeFour/ArtI/book/BookDto.java
@@ -1,4 +1,31 @@
 package threeFour.ArtI.book;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
 public class BookDto {
+
+    private Long id;
+    private String keyword;
+    private Long ImgIDs;
+    private String createdDate;
+
+    @Builder
+    public BookDto(Long id, String keyword, Long ImgIDs, String createdDate){
+        this.id = id;
+        this.keyword = keyword;
+        this.ImgIDs = ImgIDs;
+        this.createdDate = createdDate;
+    }
+
+    public Book toEntity(){
+        Book build = Book.builder()
+                .id(id)
+                .keyword(keyword)
+                .ImgIDs(ImgIDs)
+                .createdDate(createdDate)
+                .build();
+        return build;
+    }
 }
--- ArtI/src/main/java/threeFour/ArtI/styler/StylerDto.java
+++ ArtI/src/main/java/threeFour/ArtI/styler/StylerDto.java
@@ -1,4 +1,31 @@
 package threeFour.ArtI.styler;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
 public class StylerDto {
+
+    private Long id;
+    private Long originImgId;
+    private Long createdImgId;
+    private String createdDate;
+
+    @Builder
+    public StylerDto(Long id, Long originImgId, Long createdImgId, String createdDate){
+        this.id = id;
+        this.originImgId = originImgId;
+        this.createdImgId = createdImgId;
+        this.createdDate = createdDate;
+    }
+
+    public Styler toEntity(){
+        Styler build = Styler.builder()
+                .id(id)
+                .originImgId(originImgId)
+                .createdImgId(createdImgId)
+                .createdDate(createdDate)
+                .build();
+        return build;
+    }
 }
